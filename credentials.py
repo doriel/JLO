e87c81076284d89dc0402bf60e47a0822622fc8f
Doriel Fonseca <doriel3572@gmail.com>
2019-04-02 15:54:14 +0100

    Adding all files

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Adicionar as tuas informações de acesso para a API do Twitter
+
+# Consumo:
+CONSUMER_KEY    = 'p0ii9GvvWirSTXWqza5hByqvH'
+CONSUMER_SECRET = 'S0ZFv7iafWrz4KKln7IOoqYfo8TxoLB94XtJa4qAtHeibtTnpI'
+
+# Acceso:
+ACCESS_TOKEN  = '231420863-UVs2TkvjnbzKoyL21wkx3ZSUm8XnWBImGgxbvKjU'
+ACCESS_SECRET = 'v2FOebxWCl7Z0gy4QkqDHvjq1xT8JvijLrCpPGpuyd0Zx'
